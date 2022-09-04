ff6b274e70e931adcba977c8f2e7621c7387a171
lysa-thorn <lysa.thorn@gmail.com>
2022-09-04 23:00:44 +0700

    Register

--- App.js
+++ App.js
@@ -1,55 +1,133 @@
-import React, { useRef, useState } from "react";
-import { Button, DrawerLayoutAndroid, Text, StyleSheet, View } from "react-native";
+import React, { useRef } from 'react';
+import { View, Button, DrawerLayoutAndroid, TextInput, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
+import { useState } from "react";
 
 const App = () => {
-  const drawer = useRef(null);
-  const [drawerPosition] = useState("left");
+    const [drawerPosition, setDrawerPosition] = useState('left');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const validateLogin = () => {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+        if (reg.test(email) === false) {
+            Alert.alert(
+                "Message",
+                "Email not correct",
+                [
+                    {
+                        text: "OK",
+                    },
+                ]
+            );
+        } else {
+            if (password != confirmPassword) {
+                Alert.alert(
+                "Message",
+                "Invalid credential",
+                [
+                    {
+                        text: "OK",
+                    },
+                ]);
+                
+            }else{
+                Alert.alert(
+                    "Message",
+                    "Email: " + email  + "\n" + "Password: " + password,
+                    [
+                        {
+                            text: "OK",
+                        },
+                    ]);
+            }
+        }   
+    }
 
+return (
+    <View style={styles.inputview}>
+        <View style={styles.LoginTextHeader}>
+            <Text style={styles.HeaderText}>Login Now!</Text>
+        </View>
 
-  const navigationView = () => (
-    <View style={[styles.container, styles.navigationContainer]}>
-      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
-      <Button
-        title="Close drawer"
-        onPress={() => drawer.current.closeDrawer()}
-      />
-    </View>
-  );
-
-  return (
-    <DrawerLayoutAndroid
-      ref={drawer}
-      drawerWidth={300}
-      drawerPosition={drawerPosition}
-      renderNavigationView={navigationView}
-    >
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Drawer on the {drawerPosition}!
-        </Text>
-       
-        <Text style={styles.paragraph}>
-          Swipe from the side or press button below to see it!
-        </Text>
-        <Button
-          title="Open drawer"
-          onPress={() => drawer.current.openDrawer()}
+        {/* email input */}
+        <Text style={styles.TextLabel}>Email</Text>
+        <TextInput
+            style={styles.TextInput}
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => { setEmail(email) }}
         />
-      </View>
-    </DrawerLayoutAndroid>
-  );
-};
+
+        {/* input password */}
+        <Text style={styles.TextLabel}>Password</Text>
+        <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => { setPassword(password) }}
+        />
+
+        {/* input confirm password */}
+        <Text style={styles.TextLabel}>Confirm Password</Text>
+        <TextInput
+            style={styles.TextInput}
+            placeholder="Confirm Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(confirmPassword) => { setConfirmPassword(confirmPassword) }}
+        />
+        {/* button login */}
+        <TouchableOpacity
+            style={styles.loginBtn}
+            onPress={validateLogin}
+        >
+            <Text style={styles.TextBtnLogin}>LOGIN</Text>
+        </TouchableOpacity>
+        <Text style={styles.MessageText}>{message}</Text>
+    </View>
+);
+}
 
 const styles = StyleSheet.create({
-  
-  navigationContainer: {
-    backgroundColor: "#ecf0f1"
-  },
-  paragraph: {
-    padding: 16,
-    fontSize: 15,
-    textAlign: "center"
-  }
+    inputview: {
+        width: "100%",
+        padding: 30,
+        marginTop: 60,
+    },
+    TextInput: {
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: "lightblue",
+        margin: 10
+    },
+    loginBtn: {
+        width: "40%",
+        borderRadius: 10,
+        backgroundColor: "blue",
+        alignItems: "center",
+        padding: 13,
+        margin: 10,
+        marginLeft: 100,
+    },
+    TextLabel: {
+        marginLeft: 10
+    },
+    TextBtnLogin: {
+        color: "white"
+    },
+    LoginTextHeader: {
+        padding: 20,
+        alignItems: 'center'
+    },
+    HeaderText: {
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    MessageText: {
+        color: "red",
+        margin: 10
+    },
 });
-
 export default App;
