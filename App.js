b316376f3af5705ac1d95fb6ecd3536f6eebf3ea
Puthy <oputhy.student@puthisastra.edu.kh>
2022-09-11 16:02:02 +0700

    tab

--- App.js
+++ App.js
@@ -1,133 +1,18 @@
-import React, { useRef } from 'react';
-import { View, Button, DrawerLayoutAndroid, TextInput, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
-import { useState } from "react";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeStackScreen from './screens/HomeStackScreen';
+import SettingsStackScreen from './screens/SettingsStackScreen';
 
-const App = () => {
-    const [drawerPosition, setDrawerPosition] = useState('left');
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const validateLogin = () => {
-        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-        if (reg.test(email) === false) {
-            Alert.alert(
-                "Message",
-                "Email not correct",
-                [
-                    {
-                        text: "OK",
-                    },
-                ]
-            );
-        } else {
-            if (password != confirmPassword) {
-                Alert.alert(
-                "Message",
-                "Invalid credential",
-                [
-                    {
-                        text: "OK",
-                    },
-                ]);
-                
-            }else{
-                Alert.alert(
-                    "Message",
-                    "Email: " + email  + "\n" + "Password: " + password,
-                    [
-                        {
-                            text: "OK",
-                        },
-                    ]);
-            }
-        }   
-    }
-
-return (
-    <View style={styles.inputview}>
-        <View style={styles.LoginTextHeader}>
-            <Text style={styles.HeaderText}>Login Now!</Text>
-        </View>
-
-        {/* email input */}
-        <Text style={styles.TextLabel}>Email</Text>
-        <TextInput
-            style={styles.TextInput}
-            placeholder="Email"
-            placeholderTextColor="#003f5c"
-            onChangeText={(email) => { setEmail(email) }}
-        />
-
-        {/* input password */}
-        <Text style={styles.TextLabel}>Password</Text>
-        <TextInput
-            style={styles.TextInput}
-            placeholder="Password"
-            placeholderTextColor="#003f5c"
-            secureTextEntry={true}
-            onChangeText={(password) => { setPassword(password) }}
-        />
-
-        {/* input confirm password */}
-        <Text style={styles.TextLabel}>Confirm Password</Text>
-        <TextInput
-            style={styles.TextInput}
-            placeholder="Confirm Password"
-            placeholderTextColor="#003f5c"
-            secureTextEntry={true}
-            onChangeText={(confirmPassword) => { setConfirmPassword(confirmPassword) }}
-        />
-        {/* button login */}
-        <TouchableOpacity
-            style={styles.loginBtn}
-            onPress={validateLogin}
-        >
-            <Text style={styles.TextBtnLogin}>LOGIN</Text>
-        </TouchableOpacity>
-        <Text style={styles.MessageText}>{message}</Text>
-    </View>
-);
+const Tab = createBottomTabNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-    inputview: {
-        width: "100%",
-        padding: 30,
-        marginTop: 60,
-    },
-    TextInput: {
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: "lightblue",
-        margin: 10
-    },
-    loginBtn: {
-        width: "40%",
-        borderRadius: 10,
-        backgroundColor: "blue",
-        alignItems: "center",
-        padding: 13,
-        margin: 10,
-        marginLeft: 100,
-    },
-    TextLabel: {
-        marginLeft: 10
-    },
-    TextBtnLogin: {
-        color: "white"
-    },
-    LoginTextHeader: {
-        padding: 20,
-        alignItems: 'center'
-    },
-    HeaderText: {
-        fontSize: 25,
-        fontWeight: "bold",
-    },
-    MessageText: {
-        color: "red",
-        margin: 10
-    },
-});
-export default App;
---
+++ screens/SettingsStackScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+
+function SettingsStackScreen() {
+    return (
+      <View>
+        <Text style={{textAlign: 'center', marginTop: 300}}>Settings Screen</Text>
+      </View>
+    );
+}
+export default SettingsStackScreen;
